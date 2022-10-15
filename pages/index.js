07d4b191cdbaf56d731202f4fd253d7f6f91cca9
MohammedAK1991 <ahmd199106@gmail.com>
2022-10-15 11:10:59 +0100

    feat(HomePage): remove Dummy meet ups and retrieve meetups frm the mongodb databse inside the gtstaticprops server side nextjs function

--- pages/index.js
+++ pages/index.js
@@ -1,35 +1,9 @@
 import { Fragment } from 'react';
 import Head from 'next/head';
-// import { MongoClient } from 'mongodb';
+import Layout from '../components/layout/Layout';
+import { MongoClient } from 'mongodb';
 
 import MeetupList from '../components/meetups/MeetupList';
-
-const DummyMeetups = [
-  {
-    id: 'm1',
-    title: 'first meet up',
-    image:
-      'https://media-exp1.licdn.com/dms/image/C4D16AQExbIc0E-QBxA/profile-displaybackgroundimage-shrink_350_1400/0/1607611520154?e=1671062400&v=beta&t=ueXyNRhEIfJaBmu8Qr-KqjqMae5uuo4rpfgjWGi7qNo',
-    address: 'some address',
-    description: 'this is first meet up',
-  },
-  {
-    id: 'm1',
-    title: 'first meet up',
-    image:
-      'https://images.unsplash.com/photo-1453728013993-6d66e9c9123a?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8Zm9jdXN8ZW58MHx8MHx8&w=1000&q=80',
-    address: 'some address',
-    description: 'this is first meet up',
-  },
-  {
-    id: 'm1',
-    title: 'first meet up',
-    image:
-      'https://images.unsplash.com/photo-1453728013993-6d66e9c9123a?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8Zm9jdXN8ZW58MHx8MHx8&w=1000&q=80',
-    address: 'some address',
-    description: 'this is first meet up',
-  },
-];
 
 function HomePage(props) {
   return (
@@ -41,9 +15,33 @@ function HomePage(props) {
           content='Browse a huge list of highly active React meetups!'
         />
       </Head>
-      <MeetupList meetups={DummyMeetups} />;
+      <MeetupList meetups={props.meetups} />;
     </Fragment>
   );
 }
+
+export async function getStaticProps() {
+  const client = await MongoClient.connect(
+    'mongodb+srv://ahmd19910618:Sameenu17@cluster0.n6nloju.mongodb.net/meetups?retryWrites=true&w=majority',
+  );
+  const db = client.db();
+  const meetupsCollection = db.collection('meetups');
+  const meetups = await meetupsCollection.find().toArray();
+  client.close();
+  console.log(meetups);
+
+  return {
+    props: {
+      meetups: meetups.map((meetup) => ({
+        title: meetup.title,
+        address: meetup.address,
+        image: meetup.image,
+        id: meetup._id.toString(),
+      })),
+    },
+    revalidate: 1,
+  };
+}
+
 
 export default HomePage;
